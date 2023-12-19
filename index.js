41d7929810fcdf860ce0afba2cb23f97a18cae22
asrorxoja07 <asrorxojaisomiddinov21@gmail.com>
2023-12-19 21:04:28 +0500

    masalalar

---
+++ index.js
@@ -0,0 +1,145 @@
+
+// 1-masala
+
+// function  Max(son1 =10, son2=15, son3=20) {
+//     let max=1;
+//     if ( son1 < son2) {
+//         max = son2;
+//     }else{
+//         max=son1;
+//     }
+
+//     if (max < son3) {
+//         max =son3;
+//     }else{
+//         max=max;
+//     }
+    
+
+//     console.log(max);
+    
+// }
+
+
+// Max();
+
+
+
+
+
+
+
+// 3-masala
+
+// let counter=0;
+
+// function son(n=10) {
+// let i=1;
+// while(i<=n) {
+//     if ( i % 2 ==1){
+//     counter++;
+//     }
+//     i++;
+// }
+
+// return counter
+// };
+
+
+// son();
+
+// console.log(counter);
+
+
+
+
+
+
+// 4-masala
+
+
+// let counter=0;
+
+// function son(n=10){
+//     let i=1;
+//  while(i<=n){
+//     if( n % i ==0){
+//         counter++;
+  
+//   }
+//   i++;
+//  }
+ 
+// };
+
+// son();
+// console.log(counter);
+
+
+
+
+// 5-masala
+
+
+  
+// function  sum(son=15) {
+//     let i=1;
+   
+//     while( i <= son){
+//         if( son % 3 ==0 && son % 5 ==0){
+//          son **2;
+//         }else{
+//          son **3;
+            
+//         }
+//         i++;
+//     }
+
+//     return sum
+// };
+
+// sum();
+// console.log(son);
+
+
+
+
+
+
+
+// 6-misol
+
+// let max;
+// function  Max(son1=32, son2=44){
+//     let birlar = son1 % 10;
+//     let onlar = (son1 / 10) % 10;
+
+//     let birlar2 = son2 % 10;
+//     let onlar2 = (son2 / 10) % 10;
+
+//     if (birlar * onlar > birlar2 * onlar2){
+//     max = son1;
+//     }else{
+//         max=son2;
+//     }
+// };
+
+// Max();
+// console.log(' kopaytmasi kattasi ' +max);
+
+
+
+// 7-misol
+
+// function tubSon(son=11){
+//     if(son % 2 ==0) {
+//         console.log("tub son emas")
+//     }else{
+//         console.log("tub son")
+
+//     }
+// };
+
+
+
+// tubSon();
